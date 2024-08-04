66bacf5b87d80dbf30e8a6cf6b4f9b7659d7bd5f
Nevojt <nevojt@gmail.com>
2024-08-04 17:34:17 +0200

    deploy private_messages

---
+++ app/AI/sayory.py
@@ -0,0 +1,40 @@
+
+
+from openai import AsyncOpenAI
+from app.config import settings
+
+sayori_key=settings.openai_api_key
+
+client = AsyncOpenAI(
+    api_key=sayori_key
+)
+
+instruction = "Ти асистент в менеджері і твоє ім'я saory далі буде повідомлення від користувача:  "
+instruction_2 = " Твоя відповідь не повинна перевищувати 600 символів."
+async def ask_to_gpt(ask_to_chat: str) -> str:
+    try:
+        chat_completion = await client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": instruction + ask_to_chat + instruction_2,
+                }
+            ],
+            model="gpt-4o-mini",
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            n=2
+        )
+        response_1 = chat_completion.choices[0].model_dump()
+        response_1 = response_1["message"]["content"]
+        response_2 = chat_completion.choices[1].model_dump()
+        response_2 = response_2["message"]["content"]
+    
+        response = [response_1, response_2]
+        return response
+    
+    except Exception as e:
+        return "Sorry, I couldn't process your request."
--- app/config.py
+++ app/config.py
@@ -3,14 +3,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_hostname: str
+    database_hostname_company: str
     database_port: str
     database_password: str
+    database_password_company: str
     database_name: str
     database_username: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
     key_crypto: str
+    openai_api_key: str
 
     model_config = SettingsConfigDict(env_file = ".env")
 
